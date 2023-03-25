daec222ada1a989ad4ebc0d7ddfb0f51fce96828
Tariq <118397532+txriq03@users.noreply.github.com>
2023-03-25 14:13:15 +0000

    🚀First commit

--- src/app/home/head.jsx
+++
@@ -1,5 +0,0 @@
-export default function Head() {
-    return (
-        <title>Home - Aniime</title>
-    )
-}
---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+const home = () => {
+  return (
+    <Typography variant='h1' align='center'>Home</Typography>
+  )
+}
+
+export default home
