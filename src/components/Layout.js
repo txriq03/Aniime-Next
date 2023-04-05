655e4548d292d61d9cab4695f59971e22913d8aa
Tariq <118397532+txriq03@users.noreply.github.com>
2023-04-05 02:12:12 +0100

    ⭐Applied favicon to all pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,14 @@
 import Navbar from './Navbar';
+import Head from 'next/head';
 
 const Layout = ({children}) => {
+
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Navbar/>
       <div>{children}</div>
     </>
