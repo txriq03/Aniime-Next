0d12200bbfe005ccd1216620217fad82a568655c
Tariq <118397532+txriq03@users.noreply.github.com>
2023-06-30 01:24:14 +0100

    ⤵️unreversed array

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -101,7 +101,7 @@ const AnimeModal = ({setAnimeId, animeId, isModalOpen, setIsModalOpen}) => {
                 
                 {episodeList &&
                 <Box>
-                    {displayEpisodes.reverse().map(episode => {
+                    {displayEpisodes.map(episode => {
                         return (
                             <Box display='flex' onClick={() => {router.push('/watch/' + episode.id)}} flexDirection='row' key={episode.id} sx={{cursor: 'pointer'}}>
                                 <Box
